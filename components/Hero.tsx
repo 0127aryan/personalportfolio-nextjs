d02b80122025f42ff324b1b00785d03720fc4f2c
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-09-30 18:57:10 +0530

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
     <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:mx-w-[60vw] flex flex-col items-center justify-center'>
             <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-                Dynamic Web magic with Next.js
+                Aryan&apos;s Dynamic Personal Portfolio with Next.js
             </h2>
 
             <TextGenerateEffect 
