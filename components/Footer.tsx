997db7f4c2a73f423aa6bc7c91683cef635e71be
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-09-30 18:55:33 +0530

    Commit

---
+++ components/Footer.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import { link } from "fs";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me{" "}
+        </p>
+
+        <a href="mailto:aryankhandelwal@gmail.com">
+          <MagicButton
+            title="Let's get in Touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-3">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Aryan Khandelwal
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              href={profile.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                <img
+                  src={profile.img}
+                  alt={profile.id}
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
